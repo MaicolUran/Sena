540367ad0b67e77bac187719e23f6c1edd8f7684
MaicolUran <maicoluranserna18@gmail.com>
2025-03-03 18:18:10 -0500

    configuracion inicial

---
+++ src/config/connection.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const URI = 'mongodb+srv://MaicolUran18:1001765274M.L*@adso2846461.2nzqs.mongodb.net/'
+
+mongoose.connect(URI)
+
+module.exports = mongoose;
